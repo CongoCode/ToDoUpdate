7fa5a026a384b837e64dc011826abb7bda31a09e
CongoCode <benis.munganga@washburn.edu>
2017-05-06 04:30:37 -0500

    Update MasterToDo.java

--- MasterToDo.java
+++ MasterToDo.java
@@ -5,9 +5,15 @@
  */
 package ToDoUpdate;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Scanner;
 import javafx.application.Application;
 import static javafx.application.Application.launch;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
@@ -17,6 +23,7 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -27,22 +34,22 @@ import javafx.stage.Stage;
 public class MasterToDo
 {
 
-  
     private ArrayList <String> DaysofWeek = new ArrayList();
-    public ComboBox <String> chooseBox;
+    public  ComboBox <String> chooseBox;
     public static ArrayList<String> langChoice = new ArrayList();
     private ArrayList<TextArea> taDays = new ArrayList<>();
     private ArrayList<CheckBox> cbDays = new ArrayList<>();
+    
     public MasterToDo()
     {
         
-        DaysofWeek.add("Monday");
-        DaysofWeek.add("Tuesday");
-        DaysofWeek.add("Wednesday");
-        DaysofWeek.add("Thursday");
-        DaysofWeek.add("Friday");
-        DaysofWeek.add("Saturday");
-        DaysofWeek.add("Sunday");
+         DaysofWeek.add("Monday");
+         DaysofWeek.add("Tuesday");
+         DaysofWeek.add("Wednesday");
+         DaysofWeek.add("Thursday");
+         DaysofWeek.add("Friday");
+         DaysofWeek.add("Saturday");
+         DaysofWeek.add("Sunday");
     }
     
     
@@ -81,20 +88,22 @@ public class MasterToDo
         //grid pane/text enter
         GridPane pane = new GridPane();
         pane.setAlignment(Pos.CENTER);
+        pane.setStyle("-fx-background-color: DarkGrey;");
         pane.setPadding(new Insets(11.5, 12.5, 13.5, 14.5));
         pane.setHgap(5.5);
         pane.setVgap(5.5);
-        VBox chckBoxPane = new VBox(20);
         chooseBox = new ComboBox();
         langChoice.add("English");
         langChoice.add("Français");
         langChoice.add("Swahili");
         langChoice.add("Lingala");
         langChoice.add("Kirundi");
+        
+        
         //loop that adds items to ComboBox
         for(int k = 0; k < 5; k++)
              {
-                 chooseBox.getItems().addAll(langChoice.get(k));
+                chooseBox.getItems().addAll(langChoice.get(k));
              }
         chooseBox.setValue("English");
         Button addBtn = new Button("Add");
@@ -111,8 +120,7 @@ public class MasterToDo
         pane.add(clear, 2, 8);
         pane.add(save, 3, 8);
         pane.add(chooseBox, 0, 0);
-
-
+        pane.setPrefSize(1000, 600);
         
         addBtn.setOnAction(e -> SendOut());
         clear.setOnAction(e -> Clear());
@@ -122,20 +130,50 @@ public class MasterToDo
     }
     
     
-    private void ShowAdded()
-    {
-        
-    }
+   public Stage ShowAdded()
+      {
+        Stage showstage= new Stage();
+         showstage.setTitle("ShowToDo");
+         HBox showDays=new HBox();
+         VBox showtext=new VBox();
+         GridPane showPane=new GridPane();
+          
+         TextArea txt=new TextArea();
+         txt.setEditable(false);
+         Scanner getText=new Scanner("todo.txt");
+         
+         showPane.getChildren().addAll(showDays,showtext);
+         Scene show=new Scene(showPane);
+         showstage.setScene(show);
+         return showstage;
+      }
     
-    public void SendOut() 
-    {
-        
-       
-    }
+      public void SendOut() 
+      {
+        try 
+       {
+            PrintWriter outP= new PrintWriter(new File("todo.txt"));
+             for(int k=0;k<7;k++)
+                {
+                     if(cbDays.get(k).isSelected()){
+                     outP.println(""+taDays.get(k).getText());
+                }
+             outP.close();
+             }  
+         }
+        catch(FileNotFoundException ex) 
+             {
+                 System.out.println("file not found");          
+             }
+       }
 
     private void Clear() 
     {
-        
+        for(int k=0;k<7;k++)
+                {
+                     if(cbDays.get(k).isSelected())
+                     taDays.get(k).setText("");
+                }
     }
-}
+  }
 
